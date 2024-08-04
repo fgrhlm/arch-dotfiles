f9e64df17f29ef10c27377b756f962894822bf09
rcd <rcd@tokamak>
2024-08-04 03:04:51 +0300

    fixed bad ff config link path

--- dotfiles/firefox/autoconfig.js
+++ dotfiles/firefox/autoconfig.js
@@ -1,2 +1,3 @@
-pref("general.config.filename", "firefox.cfg");
+// autoconfig
 pref("general.config.obscure_value", 0);
+pref("general.config.filename", "firefox.cfg");
--- scripts/conf-ff.sh
+++ scripts/conf-ff.sh
@@ -3,7 +3,7 @@
 config_ff () {
     echo_color "${BG_BLUE}${FG_GREY}" "[${FUNCNAME}].."
     sudo cp -v $RARCH_DOTFILES/firefox/autoconfig.js /usr/lib/firefox/defaults/pref/autoconfig.js
-    sudo cp -v $RARCH_DOTFILES/firefox/config.js /usr/lib/firefox/defaults/pref/firefox.cfg
+    sudo cp -v $RARCH_DOTFILES/firefox/config.js /usr/lib/firefox/firefox.cfg
     sudo cp -v $RARCH_DOTFILES/firefox/policies.json /usr/lib/firefox/distribution/policies.json
     sudo rm -fv /usr/lib/firefox/browser/features/screenshots@mozilla.org.xpi
     sudo rm -fv /usr/lib/firefox/browser/features/formautofill@mozilla.org.xpi
