872ff244b03b6241909259d0eb2af333811318c3
rcdz <roccdcazbah@gmail.com>
2024-11-09 14:50:01 +0200

    Added lua language server to nvim lsp config

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -105,12 +105,54 @@ opt.completeopt = "menuone,noinsert,noselect"
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local lspconfig = require('lspconfig')
 
+-- Jedi
 lspconfig.jedi_language_server.setup {}
+
+-- TSServer
 lspconfig.tsserver.setup {}
+
+-- Gopls
 lspconfig.gopls.setup {}
+
+-- Clangd
 lspconfig.clangd.setup {}
-lspconfig.hls.setup{
-    filetypes = { 'haskell', 'lhaskell', 'cabal' },
+
+-- Haskell
+lspconfig.hls.setup{filetypes = { 'haskell', 'lhaskell', 'cabal' },}
+
+-- Lua Language Server
+lspconfig.lua_ls.setup {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
 }
 
 vim.api.nvim_create_autocmd('LspAttach', {
